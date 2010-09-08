a63e204d460f72931cb931d8c2ef8f59495d67b4
demoder <demoder@localhost>
2010-09-08 02:27:29 +0000

    Bumped version to 1.0.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Common")]
+[assembly: AssemblyTitle("Demoder.Common")]
 [assembly: AssemblyDescription("Contains methods and classes used by other projects in the Demoders Tools namespace")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Common")]
+[assembly: AssemblyProduct("Demoder.Common")]
 [assembly: AssemblyCopyright("Copyright © Demoder (demoder@flw.nu) 2010. See license.txt for more information")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.1")]
-[assembly: AssemblyFileVersion("1.0.2.1")]
+[assembly: AssemblyVersion("1.0.2.14")]
+[assembly: AssemblyFileVersion("1.0.2.14")]
