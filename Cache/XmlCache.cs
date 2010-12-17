de78526f82c9988444906e297d6c589fe8722b17
demoder <demoder@localhost>
2010-12-17 13:19:43 +0000

    Updated license headers with new URL

--- Cache/XmlCache.cs
+++ Cache/XmlCache.cs
@@ -24,7 +24,7 @@
 
 /*
 Demoder.Common
-Copyright (c) 2010 Demoder <demoder@flw.nu> (project: http://trac.flw.nu/demoder.common/)
+Copyright (c) 2010 Demoder <demoder@flw.nu> (project: http://redmine.flw.nu/projects/demoder-common/)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
