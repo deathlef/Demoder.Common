0012770b8e90b73828a426255e6731418e87ab8e
Savagedlight <Savagedlight@localhost>
2011-07-22 13:14:43 +0200

    Added Regex extension method which returns a dictionary with key:subPatternName, value: subPatternMatch for every match in the MatchCollection.

---
+++ Extensions/RegexExtensions.cs
@@ -0,0 +1,50 @@
+﻿/*
+Demoder.Common
+Copyright (c) 2010,2011 Demoder <demoder@demoder.me>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Demoder.Common.Extensions
+{
+    public static class RegexExtensions
+    {
+       public static List<Dictionary<string, string>> GetMatchCollection(this Regex regex, string inputText)
+        {
+            var ret = new List<Dictionary<string, string>>();
+            foreach (Match match in regex.Matches(inputText))
+            {
+                var dict = new Dictionary<string, string>();
+                for (int i = 1; i < match.Groups.Count; i++)
+                {
+                    Group group = match.Groups[i];
+                    dict[regex.GroupNameFromNumber(i)] = group.Value;
+                }
+                ret.Add(dict);
+            }
+            return ret;
+        }
+    }
+}
