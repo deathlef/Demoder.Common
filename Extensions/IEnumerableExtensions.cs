daf761f40808ce64aa0452ae12d255efbf5e955c
Marie Helene Kvello-Aune <752131+Savagedlight@users.noreply.github.com>
2011-10-01 22:58:56 +0200

    Now handles empty IEnumerables too.

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -7,8 +7,15 @@ namespace Demoder.Common.Extensions
 {
     public static class IEnumerableExtensions
     {
+        /// <summary>
+        /// Returns a random entry (or default if empty) from the IEnumerable instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public static T Random<T>(this IEnumerable<T> obj)
         {
+            if (obj.Count() == 0) { return default(T); }
             int maxPos = obj.Count() -1;
             int pos = Misc.NewRandom().Next(0, maxPos);
             return obj.ElementAt(pos);
