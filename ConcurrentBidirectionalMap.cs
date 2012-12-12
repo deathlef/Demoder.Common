375255c394c9043ada5f5fac7ec890ae4fb0e5e3
Savagedlight <Savagedlight@localhost>
2012-12-12 01:33:19 +0100

    ConcurrentBidirectionalMap: - Renamed BidirectionalMapStoreResult to MapAlteration - Renamed .TryStore() to .Store() - Documented all methods - Added new method .TryStore() which will return false if either key or value already exist. - Fixed errornous return value of .Store() - Added new methods ContainsKey(TKey), ContainsValue(TValue), Contains(TKey,TValue) - Added new methods bool TryRemoveKey(TKey, out TValue), bool TryRemoveValue(TValue, out TKey) - Added new method bool TryRemove(TKey, TValue) - Added new debugging method TestIntegrity()

--- ConcurrentBidirectionalMap.cs
+++ ConcurrentBidirectionalMap.cs
@@ -24,20 +24,40 @@ THE SOFTWARE.
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 
 namespace Demoder.Common
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TKey">Type of key to be stored</typeparam>
+    /// <typeparam name="TValue">Type of value to be stored</typeparam>
     public class ConcurrentBidirectionalMap<TKey, TValue>
     {
+        /// <summary>
+        /// Contains a TKey to TValue relation
+        /// </summary>
         private ConcurrentDictionary<TKey, TValue> keyValueMap;
+        /// <summary>
+        /// Contains a TValue to TKey relation
+        /// </summary>
         private ConcurrentDictionary<TValue, TKey> valueKeyMap;
+
         private ReaderWriterLockSlim locker;
 
-        IEqualityComparer<TKey> keyComparer;
-        IEqualityComparer<TValue> valueComparer;
+        /// <summary>
+        /// Used to perform comparisons on TKey
+        /// </summary>
+        private IEqualityComparer<TKey> keyComparer;
+        /// <summary>
+        /// Used to perform comparisons on TValue
+        /// </summary>
+        private IEqualityComparer<TValue> valueComparer;
 
         public int Count
         {
@@ -55,6 +75,12 @@ namespace Demoder.Common
             }
         }
 
+
+        /// <summary>
+        /// Initializes a new ConcurrentBidirectionalMap that is empty
+        /// </summary>
+        /// <param name="keyComparer">IEqualityComparer to compare keys, or null for default.</param>
+        /// <param name="valueComparer">IEqualityComparer to compare values, or null for default.</param>
         public ConcurrentBidirectionalMap(IEqualityComparer<TKey> keyComparer=null, IEqualityComparer<TValue> valueComparer=null)
         {
             this.locker = new ReaderWriterLockSlim();
@@ -82,8 +108,104 @@ namespace Demoder.Common
             this.valueKeyMap = new ConcurrentDictionary<TValue, TKey>(this.valueComparer);
         }
 
+        /// <summary>
+        /// Determines whether the <paramref name="key"/> exists
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <returns>true if <paramref name="key"/> is found, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">Key is a null reference</exception>
+        public bool ContainsKey(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            this.locker.EnterReadLock();
+            try
+            {
+                return this.keyValueMap.ContainsKey(key);
+            }
+            finally
+            {
+                this.locker.ExitReadLock();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the <paramref name="value"/> exists
+        /// </summary>
+        /// <param name="value">The value to locate</param>
+        /// <returns>true if <paramref name="value"/> is found, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">value is a null reference</exception>
+        public bool ContainsValue(TValue value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            this.locker.EnterReadLock();
+            try
+            {
+                return this.valueKeyMap.ContainsKey(value);
+            }
+            finally
+            {
+                this.locker.ExitReadLock();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the <paramref name="key" /> and <paramref name="value"/> exists, and if they are associated.
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <param name="value">The value to locate</param>
+        /// <returns>true if key and value exist and are associated with eachother. Otherwise false.</returns>
+        /// <exception cref="System.ArgumentNullException">key or value is a null reference</exception>
+        public bool Contains(TKey key, TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            this.locker.EnterReadLock();
+            try
+            {
+                if (!this.keyValueMap.ContainsKey(key))
+                {
+                    return false;
+                }
+                if (!this.valueComparer.Equals(this.keyValueMap[key], value))
+                {
+                    return false;
+                }
+                return true;
+            }
+            finally
+            {
+                this.locker.ExitReadLock();
+            }
+        }
+
+        /// <summary>
+        /// Attempts to get the value associated with the specified <paramref name="key"/>
+        /// </summary>
+        /// <param name="key">The key of the value to get</param>
+        /// <param name="value">When this method returns, value contains the object with the specified value or the default value of <typeparamref name="TValue"/>, if the operation failed.</param>
+        /// <returns>True if the key was found, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">Key is a null reference</exception>
         public bool TryGetValue(TKey key, out TValue value)
         {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
             this.locker.EnterReadLock();
             try
             {
@@ -95,8 +217,20 @@ namespace Demoder.Common
             }
         }
 
+        /// <summary>
+        /// Attempts to get the key associated with the specified <paramref name="value"/>
+        /// </summary>
+        /// <param name="value">The value of the key to get</param>
+        /// <param name="key">When this method returns, key contains the object with the specified key or the default value of <typeparamref name="TKey"/>, if the operation failed.</param>
+        /// <returns>True if the value was found, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">Key is a null reference</exception>
         public bool TryGetKey(TValue value, out TKey key)
         {
+            if (value == null) 
+            { 
+                throw new ArgumentNullException("value"); 
+            }
+
             this.locker.EnterReadLock();
             try
             {
@@ -108,11 +242,27 @@ namespace Demoder.Common
             }
         }
 
-
-        public BidirectionalMapStoreResult TryStore(TKey key, TValue value)
+        /// <summary>
+        /// Stores a key/value combination, removing other key/value combination if necessary.
+        /// </summary>
+        /// <param name="key">The key to add</param>
+        /// <param name="value">The value to add</param>
+        /// <returns>An enumerator describing which alterations were made, if any.</returns>
+        /// <exception cref="System.ArgumentNullException">key or value is a null reference.</exception>
+        public MapAlteration Store(TKey key, TValue value)
         {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
             this.locker.EnterWriteLock();
-            BidirectionalMapStoreResult returnValue = BidirectionalMapStoreResult.Unmodified;
+            MapAlteration returnValue = MapAlteration.Unmodified;
             try
             { 
                 bool haveKey = this.keyValueMap.ContainsKey(key);
@@ -131,11 +281,11 @@ namespace Demoder.Common
                     // Remove old value and ignore old values key, because it's the current key.
                     TKey oldValueKey;
                     this.valueKeyMap.TryRemove(oldValue, out oldValueKey);
-                    returnValue |= BidirectionalMapStoreResult.RemovedValue;
+                    returnValue |= MapAlteration.RemovedValue;
 
                     // Update key.
                     this.keyValueMap[key] = value;
-                    returnValue |= BidirectionalMapStoreResult.ModifiedValue;
+                    returnValue |= MapAlteration.ModifiedKey;
                 }
 
                 bool haveValue = this.valueKeyMap.ContainsKey(value);
@@ -146,20 +296,20 @@ namespace Demoder.Common
                     // Remove old key. Ignore value, as we know that's our current value.
                     TValue oldKeyValue;
                     this.keyValueMap.TryRemove(oldKey, out oldKeyValue);
-                    returnValue |= BidirectionalMapStoreResult.RemovedKey;
+                    returnValue |= MapAlteration.RemovedKey;
                     // Remove old value map.
                     this.valueKeyMap.TryRemove(value, out oldKey);
-                    returnValue |= BidirectionalMapStoreResult.ModifiedKey;
+                    returnValue |= MapAlteration.ModifiedValue;
                 }
                 else
                 {
-                    returnValue |= BidirectionalMapStoreResult.AddedValue;
+                    returnValue |= MapAlteration.AddedValue;
                 }
 
                 if (!haveKey)
                 {
                     this.keyValueMap[key] = value;
-                    returnValue |= BidirectionalMapStoreResult.AddedKey;
+                    returnValue |= MapAlteration.AddedKey;
                 }
                 this.valueKeyMap[value] = key;
                 return returnValue;
@@ -170,36 +320,248 @@ namespace Demoder.Common
                 this.locker.ExitWriteLock();
             }
         }
+
+
+        /// <summary>
+        /// Attempts to add a specified key and value to the map
+        /// </summary>
+        /// <param name="key">The key to add</param>
+        /// <param name="value">The value to add</param>
+        /// <returns>true if the key/value was successfully added. false if key or value already exist.</returns>
+        /// <exception cref="System.ArgumentNullException">key or value is a null reference</exception>
+        public bool TryStore(TKey key, TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+            
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            this.locker.EnterWriteLock();
+            try
+            {
+                if (this.keyValueMap.ContainsKey(key))
+                {
+                    return false;
+                }
+                if (this.valueKeyMap.ContainsKey(value))
+                {
+                    return false;
+                }
+                this.keyValueMap[key] = value;
+                this.valueKeyMap[value] = key;
+                return true;
+            }
+            finally
+            {
+                this.locker.ExitWriteLock();
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove the specified key and associated value, returning the value.
+        /// </summary>
+        /// <param name="key">The key of the element to remove and return</param>
+        /// <param name="value">Value associated with key</param>
+        /// <returns>True if a mapping was removed, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">key is a null reference</exception>
+        public bool TryRemoveKey(TKey key, out TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            this.locker.EnterWriteLock();
+            try
+            {
+                if (!this.keyValueMap.TryRemove(key, out value))
+                {
+                    return false;
+                }
+                TKey tmp;
+                this.valueKeyMap.TryRemove(value, out tmp);
+                return true;
+            }
+            finally
+            {
+                this.locker.ExitWriteLock();
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove the specified value and associated key, returning the key.
+        /// </summary>
+        /// <param name="value">The value of the element to remove and return</param>
+        /// <param name="key">Key associated with value</param>
+        /// <returns>True if a mapping was removed, otherwise false</returns>
+        /// <exception cref="System.ArgumentNullException">value is a null reference</exception>
+        public bool TryRemoveValue(TValue value, out TKey key)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            this.locker.EnterWriteLock();
+            try
+            {
+                if (!this.valueKeyMap.TryRemove(value, out key))
+                {
+                    return false;
+                }
+                TValue tmp;
+                this.keyValueMap.TryRemove(key, out tmp);
+                return true;
+            }
+            finally
+            {
+                this.locker.ExitWriteLock();
+            }
+        }
+
+        /// <summary>
+        /// Removes a key=value combination, but only if key and value exist, and they're associated with eachother.
+        /// </summary>
+        /// <param name="key">Key to remove</param>
+        /// <param name="value">Value to remove</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException">key or value is a null reference</exception>
+        public bool TryRemove(TKey key, TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("key");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            this.locker.EnterWriteLock();
+            try
+            {
+                if (!this.keyValueMap.ContainsKey(key))
+                {
+                    // Don't have key
+                    return false;
+                }
+                if (!this.valueComparer.Equals(this.keyValueMap[key], value))
+                {
+                    // Value isn't what we want to remove
+                    return false;
+                }
+                // Remove key and value
+                TKey outKey;
+                TValue outValue;
+                if (!this.keyValueMap.TryRemove(key, out outValue))
+                {
+                    throw new InvalidDataException("Have key but can't remove it. Key: " + key.ToString());
+                }
+                if (!this.valueKeyMap.TryRemove(value, out outKey))
+                {
+                    throw new InvalidDataException("Have value but can't remove it. Value: " + value.ToString());
+                }
+                return true;
+            }
+            finally
+            {
+                this.locker.ExitWriteLock();
+            }
+        }
+
+        /// <summary>
+        /// Tests the integrity of this instance.
+        /// </summary>
+        /// <returns></returns>
+        [Obsolete("This method is only used for unit tests during development of this class, and will be removed in a future version.")]
+        public bool TestIntegrity()
+        {
+            this.locker.EnterReadLock();
+            try
+            {
+                foreach (var kvp in this.keyValueMap)
+                {
+                    if (!this.valueKeyMap.ContainsKey(kvp.Value))
+                    {
+                        return false;
+                    }
+                    if (!this.keyComparer.Equals(this.valueKeyMap[kvp.Value], kvp.Key))
+                    {
+                        return false;
+                    }
+                }
+
+                foreach (var kvp in this.valueKeyMap)
+                {
+                    if (!this.keyValueMap.ContainsKey(kvp.Value))
+                    {
+                        return false;
+                    }
+                    if (!this.valueComparer.Equals(this.keyValueMap[kvp.Value], kvp.Key))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            finally
+            {
+
+                this.locker.ExitReadLock();
+            }
+        }
     }
 
     [Flags]
-    public enum BidirectionalMapStoreResult : uint
+    public enum MapAlteration : uint
     {
         /// <summary>
         /// No changes were made
         /// </summary>
         Unmodified = BitFlag.None,
+
         /// <summary>
-        /// A value had its key association updated
+        /// An existing value received a new key.
         /// </summary>
+        /// <remarks>
+        /// This is often paired with <see cref="MapAlteration.RemoveKey" /> as the old key is no longer associated with a value.
+        /// </remarks>
         ModifiedValue = BitFlag.Bit0,
+        
         /// <summary>
         /// A value was removed
         /// </summary>
         RemovedValue = BitFlag.Bit1,
-        /// <summary>
-        /// A key had its value association modified
-        /// </summary>
-        ModifiedKey=BitFlag.Bit2,
-        /// <summary>
-        /// A key was removed
-        /// </summary>
-        RemovedKey = BitFlag.Bit3,
 
         /// <summary>
         /// A value was added
         /// </summary>
-        AddedValue = BitFlag.Bit4,
+        AddedValue = BitFlag.Bit2,
+        
+
+
+        /// <summary>
+        /// An existing key received a new value.
+        /// </summary>
+        /// <remarks>
+        /// This is often paired with <see cref="MapAlteration.RemovedValue"/> as the old value is no longer associated with a key.
+        /// </remarks>
+        ModifiedKey=BitFlag.Bit3,
+        
+        /// <summary>
+        /// An existing key was removed.
+        /// </summary>
+        /// <remarks>
+        /// This is often paired with <see cref="MapAlteration.RemovedValue"/> or <see cref="MapAlteration.ModifiedValue"/>
+        /// </remarks>
+        RemovedKey = BitFlag.Bit4,
+
         /// <summary>
         /// A key was added
         /// </summary>
