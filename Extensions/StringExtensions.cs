7dfafc66dbe987c0354f7618e6d34e7d6a48ddee
Marie Helene Kvello-Aune <752131+Savagedlight@users.noreply.github.com>
2013-03-29 07:55:20 +0100

    Added a string extension which enforces a first uppercase character followed by lowercase characters.

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,61 @@
+﻿/*
+Demoder.Common
+Copyright (c) 2010-2013 Demoder <demoder@demoder.me>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demoder.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts first character to uppercase, and the remaining to lowercase. Special characters and numbers are passed as-is.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string UppercaseFirst(this string str)
+        {
+            string input = str.ToLowerInvariant();
+            StringBuilder sb = new StringBuilder();
+            bool first = true;
+            foreach (char c in input)
+            {
+                if (c >= 'A' && c <= 'Z' && !first)
+                {
+                    sb.Append((char)(c + 32));
+                }
+                else if (c >= 'a' && c <= 'z' && first)
+                {
+                    sb.Append((char)(c - 32));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
